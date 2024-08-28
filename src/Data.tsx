02f5db280b2bf5d2713f3e3a4ebee5ab02071b5b
Haley B-E <hbrandterichsen@gmail.com>
2024-08-27 21:37:30 -0400

    added scraper and data handling for esh

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,12 +1,14 @@
 import { ISchedule } from "./types";
 // data folder not synced to github, add the file and paste scraped data into it
 import boulderingProjectData from "./data/boulderingproject.json";
+import eshData from "./data/esh.json";
 import { DayPilot } from "@daypilot/daypilot-lite-react";
 
 export const getParsedData = (): ISchedule[] => {
     const boulderingProjectSchedule = getBoulderingProjectSchedule();
+    const eshSchedule = getEshSchedule();
 
-    return [boulderingProjectSchedule];
+    return [boulderingProjectSchedule, eshSchedule];
 }
 
 const convertToDayPilotDate = (date: string): DayPilot.Date => new DayPilot.Date(new Date(date),true);
@@ -26,6 +28,25 @@ const getBoulderingProjectSchedule = (): ISchedule => {
         source: 'Bouldering Project',
         color: '#3498db',
         events: parsedBoulderingProjectData
+    }
+}
+
+const getEshSchedule = (): ISchedule => {
+    const parsedEshData: DayPilot.EventData[] = eshData.map(ev => {
+        console.log(ev)
+        return {
+            id: ev.id,
+            text: ev.name.split('-')[0],
+            description: ev.description,
+            start: convertToDayPilotDate(ev.startTime),
+            end: convertToDayPilotDate(ev.endTime),
+            toolTip: ev.description
+        }
+    }).sort((x,y) => x.start.getTime() - y.start.getTime());
+    return {
+        source: 'Esh',
+        color: '#e74c3c',
+        events: parsedEshData
     }
 }
 
--- src/scrapers/esh.js
+++ src/scrapers/esh.js
@@ -1,2 +1,19 @@
 //general: https://app.amilia.com/store/en/eshcircusarts/api/Program/List
 // current makeup calendar: https://app.amilia.com/store/en/eshcircusarts/shop/programs/calendar/106220?view=month&scrollToCalendar=true
+
+// in sources, amilia.store-libs.min.js
+//e.isStart && (e = this.getEventTimeText(i)) && (o = '<span class="fc-time">' + m(e) + "</span>"),
+
+eventsOfInterest = e.filter(ev => !ev.hasPassed && ev.AttendanceString !== "Full");
+output = eventsOfInterest.map(ev => {
+    return {
+        id: ev.SegmentId,
+        name: ev.ActivityName,
+        description: ev.ActivityName,
+        startTime: ev.start._i,
+        endTime: ev.end._i
+    }
+});
+
+// make sure you've clicked into body of the page, otherwise this fails
+setTimeout(async()=>await window.navigator.clipboard.writeText(JSON.stringify(output)), 3000);
