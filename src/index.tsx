04cbe527102a0d0e800ebcfaa6aaad8894222441
Haley B-E <hbrandterichsen@gmail.com>
2024-08-29 23:15:56 -0400

    more code cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Calendar from './Calendar';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='App'>
+      <h1>Consolidated Fitness Calendar 💪</h1>
+      <Calendar/>
+    </div>
   </React.StrictMode>
 );
